87b88cbe5757c457d4bf47f0c84a376e7855163d
maxu <maxu@tal.com>
2020-12-10 16:19:46 +0800

    android增加截图

--- android/src/main/kotlin/com/tal/d_stack_spy/DStackSpyPlugin.kt
+++ android/src/main/kotlin/com/tal/d_stack_spy/DStackSpyPlugin.kt
@@ -1,36 +1,96 @@
 package com.tal.d_stack_spy
 
+import android.app.Activity
+import android.app.Application
+import android.app.Application.ActivityLifecycleCallbacks
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.util.*
 
 /** DStackSpyPlugin */
-class DStackSpyPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class DStackSpyPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "d_stack_spy")
-    channel.setMethodCallHandler(this)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "d_stack_spy")
+        channel.setMethodCallHandler(this)
+        var context = flutterPluginBinding.applicationContext as Application
+        context.registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity) {
+            }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+            override fun onActivityStarted(activity: Activity) {
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+            }
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                this@DStackSpyPlugin.activity = activity
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                this@DStackSpyPlugin.activity = activity
+            }
+        })
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "spySendScreenShotActionToNative") {
+            val arguments = call.arguments as Map<*, *>
+            handleSpySendScreenShotActionToNative(arguments)
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    private fun handleSpySendScreenShotActionToNative(arguments: Map<*, *>?) {
+        if (arguments == null) {
+            return
+        }
+        val target: String = arguments["target"] as String
+        if (TextUtils.isEmpty(target)) {
+            return
+        }
+        var img = SnapShotUtils.getImgBase64(activity)
+        if (TextUtils.isEmpty(img)) {
+            img = ""
+        } else {
+            img = "data:image/png;base64,$img"
+        }
+        spyReceiveScreenShotFromNative(target, img)
+
+    }
+
+    private fun spyReceiveScreenShotFromNative(target: String, img: String) {
+        Log.e("dstackspy", "spyReceiveScreenShotFromNative")
+        val resultMap: HashMap<String?, Any?> = HashMap<String?, Any?>()
+        resultMap["target"] = target
+        resultMap["imageData"] = img
+        channel.invokeMethod("spyReceiveScreenShotFromNative", resultMap)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
---
+++ android/src/main/kotlin/com/tal/d_stack_spy/SnapShotUtils.java
@@ -0,0 +1,82 @@
+package com.tal.d_stack_spy;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import java.io.ByteArrayOutputStream;
+
+public class SnapShotUtils {
+
+    public static String getImgBase64(Activity act) {
+        Bitmap scrBmp = takeScreenShot(act);
+        if (scrBmp == null) {
+            return "";
+        }
+        String imgBase64 = getImgBase64(scrBmp);
+        if (TextUtils.isEmpty(imgBase64)) {
+            return "";
+        }
+        return imgBase64;
+    }
+
+    public static Bitmap takeScreenShot(Activity activity) {
+        if (activity == null || activity.isFinishing()) {
+            return null;
+        }
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bmp = view.getDrawingCache();
+        int statusBarHeight = getStatusBarHeight(activity);
+        int width = (int) getDeviceDisplaySize(activity)[0];
+        int height = (int) getDeviceDisplaySize(activity)[1];
+        Bitmap ret = Bitmap.createBitmap(bmp, 0, statusBarHeight, width, height - statusBarHeight);
+        view.destroyDrawingCache();
+        return ret;
+    }
+
+    public static String getImgBase64(Bitmap scrBmp) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        scrBmp.compress(Bitmap.CompressFormat.PNG, 100, bos);
+        byte[] bb = bos.toByteArray();
+        String image = Base64.encodeToString(bb, Base64.NO_WRAP);
+        return image;
+    }
+
+    public static float[] getDeviceDisplaySize(Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        float[] size = new float[2];
+        size[0] = width;
+        size[1] = height;
+
+        return size;
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int height = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            height = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return height;
+    }
+
+    /**
+     * Bitmap对象是否为空。
+     */
+    public static boolean isEmptyBitmap(Bitmap src) {
+        return src == null || src.getWidth() == 0 || src.getHeight() == 0;
+    }
+
+}
